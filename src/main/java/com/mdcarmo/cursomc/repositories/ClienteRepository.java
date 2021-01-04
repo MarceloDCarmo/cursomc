f80ed95fd583099486919d15a585951ce1859225
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-04 18:17:04 -0300

    Criadas classes Cliente, TipoCliente, Telefone e Endereco e seus repositories

--- src/main/java/com/mdcarmo/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mdcarmo/cursomc/repositories/ClienteRepository.java
@@ -3,9 +3,9 @@ package com.mdcarmo.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mdcarmo.cursomc.domain.Cidade;
+import com.mdcarmo.cursomc.domain.Cliente;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
 }
--- src/main/java/com/mdcarmo/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/mdcarmo/cursomc/repositories/EnderecoRepository.java
@@ -3,9 +3,9 @@ package com.mdcarmo.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mdcarmo.cursomc.domain.Cidade;
+import com.mdcarmo.cursomc.domain.Endereco;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
 	
 }
