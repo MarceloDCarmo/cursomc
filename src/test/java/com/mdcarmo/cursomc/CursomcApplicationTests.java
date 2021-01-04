967b307bad4d84e4db4f3526d686ac1d65c4f97f
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-04 13:43:51 -0300

    Projeto criado

---
+++ src/test/java/com/mdcarmo/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdcarmo.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
