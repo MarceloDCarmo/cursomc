4fe76cf2d7ca148a8e3d293ce63fc831ab69ff87
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-04 15:51:00 -0300

    Criando operação de instanciação

--- src/main/java/com/mdcarmo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mdcarmo/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,11 @@
 package com.mdcarmo.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.mdcarmo.cursomc.domain.Categoria;
 import com.mdcarmo.cursomc.services.CategoriaService;
