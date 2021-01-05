5f3a0b4bb6c0e11f83d9d233ff85b18cf0384a33
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-05 20:33:27 -0300

    Adicionados métodos de subtotal e valor total de pedidos

--- src/main/java/com/mdcarmo/sprgbappbackend/domain/Pedido.java
+++ src/main/java/com/mdcarmo/sprgbappbackend/domain/Pedido.java
@@ -52,6 +52,14 @@ public class Pedido implements Serializable {
 		this.enderecoEntrega = enderecoEntrega;
 	}
 
+	public Double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido item : itens) {
+			soma += item.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
