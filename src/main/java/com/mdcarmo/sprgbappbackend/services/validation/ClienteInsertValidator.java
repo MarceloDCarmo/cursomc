93efbcacf83b662c0ac34714ed17b58320eca1ae
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-05 16:47:07 -0300

    Validação de CPF e CNPJ na inserção de Cliente

---
+++ src/main/java/com/mdcarmo/sprgbappbackend/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,36 @@
+package com.mdcarmo.sprgbappbackend.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.mdcarmo.sprgbappbackend.domain.enums.TipoCliente;
+import com.mdcarmo.sprgbappbackend.dto.ClienteNewDTO;
+import com.mdcarmo.sprgbappbackend.resources.exception.FieldErrorMessage;
+import com.mdcarmo.sprgbappbackend.services.validation.util.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldErrorMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCpf(objDto.getCpfOuCnpj()))
+			list.add(new FieldErrorMessage("cpfOuCnpj", "CPF inválido!"));
+			
+		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj()))
+			list.add(new FieldErrorMessage("cpfOuCnpj", "CNPJ inválido!"));
+		
+		for (FieldErrorMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
