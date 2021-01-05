ef403285a3288d6ccaaef9ee42676c12ee2b802f
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-05 12:40:15 -0300

    Paginação de categorias com parâmetros opicionais na requisição

--- src/main/java/com/mdcarmo/sprgbappbackend/resources/CategoriaResource.java
+++ src/main/java/com/mdcarmo/sprgbappbackend/resources/CategoriaResource.java
@@ -2,10 +2,10 @@ package com.mdcarmo.sprgbappbackend.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,6 +39,17 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> categorias = service.findAll();
 		List<CategoriaDTO> categoriasDTO = categorias.stream().map(cat -> new CategoriaDTO(cat)).collect(Collectors.toList());
+		return ResponseEntity.ok(categoriasDTO);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction){
+		Page<Categoria> categorias = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> categoriasDTO = categorias.map(cat -> new CategoriaDTO(cat));
 		return ResponseEntity.ok(categoriasDTO);
 	}
 	
