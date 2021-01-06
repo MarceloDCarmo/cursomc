3d5e10e15d5d5dc0623ae49c9044918b5666280c
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-06 19:35:16 -0300

    Criado profile de testes

---
+++ src/main/java/com/mdcarmo/sprgbappbackend/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.mdcarmo.sprgbappbackend.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mdcarmo.sprgbappbackend.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateDatabase();
+		return true;
+	}
+	
+}
