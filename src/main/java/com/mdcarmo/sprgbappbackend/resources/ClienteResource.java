df1b611aed4d1214f18275408c7881c0a60f3594
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-12 21:28:35 -0300

    Autorizados endpoints para perfis específicos

--- src/main/java/com/mdcarmo/sprgbappbackend/resources/ClienteResource.java
+++ src/main/java/com/mdcarmo/sprgbappbackend/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,6 +39,7 @@ public class ClienteResource {
 		return ResponseEntity.ok(cat);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> clientes = service.findAll();
@@ -63,7 +65,8 @@ public class ClienteResource {
 		cliente = service.update(cliente);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
