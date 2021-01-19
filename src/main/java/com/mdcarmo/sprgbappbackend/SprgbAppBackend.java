78bad3cbabe1964182c2e25f58150424da569612
Marcelo Dias <marcelodkarmo@gmail.com>
2021-01-19 14:30:28 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/mdcarmo/sprgbappbackend/SprgbAppBackend.java
+++ src/main/java/com/mdcarmo/sprgbappbackend/SprgbAppBackend.java
@@ -1,11 +1,17 @@
 package com.mdcarmo.sprgbappbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mdcarmo.sprgbappbackend.services.S3Service;
+
 @SpringBootApplication
 public class SprgbAppBackend implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SprgbAppBackend.class, args);
@@ -14,5 +20,6 @@ public class SprgbAppBackend implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3service.uploadFile("D:\\Marcelo\\Imagens\\mustang97.jpg");
 	}
 }
